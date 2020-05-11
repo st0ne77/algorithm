c49fed6b71cf31bd7f1c5ad21b7a676a0475fc88
st0ne77 <951251531@qq.com>
2020-05-11 20:39:31 +0800

    add bubble description

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -1,7 +1,16 @@
 #include <QCoreApplication>
 #include <QVector>
-#include <QDebug>
+#include <iostream>
 #include <QTextCodec>
+
+template<typename T>
+void printVector(const std::vector<T>& vct){
+    for(unsigned int i = 0; i < vct.size(); ++i){
+        std::cout << vct[i] << ",";
+    }
+    std::cout << std::endl;
+}
+
 template<typename T>
 std::vector<T> bubbleSort(std::vector<T> src){
     if(src.size() < 2)
@@ -9,7 +18,7 @@ std::vector<T> bubbleSort(std::vector<T> src){
     bool swaped = false;
     int count = 0;
     do{
-        qDebug() << "   " << QString("--%1--").arg(++count);
+        std::cout << "   " << QString("--%1--").arg(++count).toStdString() << std::endl;
         swaped = false;
         for(auto it = src.begin(); (it+1) != src.end(); ++it){
             auto current = it;
@@ -18,7 +27,8 @@ std::vector<T> bubbleSort(std::vector<T> src){
                 swaped = true;
                 std::swap(*current, *next);
             }
-            qDebug() << "       " << QString("----%1----").arg(it - src.begin() + 1) << QVector<T>::fromStdVector(src);
+            std::cout << "       " << QString("----%1----").arg(it - src.begin() + 1).toStdString();
+            printVector(src);
         }
     }while(swaped /*&& count < (src.size() - 1)*/);
     return src;
@@ -28,9 +38,11 @@ int main(int argc, char *argv[]){
     QCoreApplication a(argc, argv);
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("utf-8"));
     //使用QVector 便于打印
-    QVector<int> src{98, 58, 56, 32, 22, 10, 1};
-    qDebug() << "src:" << src;
-    std::vector<int> dest = bubbleSort(src.toStdVector());
-    qDebug() << "dest:" <<QVector<int>::fromStdVector(dest);
+    std::vector<int> src{98, 58, 56, 32, 22, 10, 1};
+    std::cout << "src:";
+    printVector(src);
+    std::vector<int> dest = bubbleSort(src);
+    std::cout << "dest:";
+    printVector(dest);
     return a.exec();
 }
